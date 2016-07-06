33978f749059e807ecd4463943884544ad510861
Nicolas Sims <sableye.nico@gmail.com>
2016-07-05 21:08:56 -0400

    Added Thank You.

--- app.js
+++ app.js
@@ -11,40 +11,41 @@ class app {
 
     static loadServer() {
         const HTTP = require('http'),
-            PORT = process.env.PORT || 8080,
-            SERVER = HTTP.createServer(function (req, res) {
-                let httpHandler = function (err, str, contentType) {
+            PORT = 1337,
+            SERVER = HTTP.createServer( (req, res) => {
+                let httpHandler = function(err, str, contentType) {
                     if (err) {
-                        res.writeHead(500, {'Content-Type': 'text/plain'});
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
                         res.end('An error has occurred: ' + err.message);
                     } else if (contentType.indexOf('image') >= 0) {
-                        res.writeHead(200, {'Content-Type': contentType});
+                        res.writeHead(200, { 'Content-Type': contentType });
                         res.end(str, 'binary');
                     } else {
-                        res.writeHead(200, {'Content-Type': contentType});
+                        res.writeHead(200, { 'Content-Type': contentType });
                         res.end(str);
                     }
                 };
-
                 if (req.method == 'POST') {
                     if (req.headers['x-requested-with'] === 'formWriteRequest') {
                         app.writeFormData(req, res);
                     } else if (req.headers['x-requested-with'] === 'getThankYouPageRequest') {
                         app.getThankYouPage(res);
+                    } else {
+                        console.log("[405] " + req.method + " to " + req.url);
+                        res.writeHead(405, "Method not supported", { 'Content-Type': 'text/html' });
+                        res.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>');
                     }
-                } else if (req.url.indexOf('/data/') >= 0) {
-                    app.render(req.url.slice(1), 'text/csv', httpHandler, 'utf-8');
                 } else if (req.url.indexOf('/javascripts/') >= 0) {
                     app.render(req.url.slice(1), 'application/ecmascript', httpHandler, 'utf-8');
                 } else if (req.url.indexOf('/css/') >= 0) {
                     app.render(req.url.slice(1), 'text/css', httpHandler, 'utf-8');
-                } else if (req.url.indexOf('/img/') >= 0) {
+                } else if (req.url.indexOf('/images/') >= 0) {
                     app.render(req.url.slice(1), 'image/jpeg', httpHandler, 'binary');
                 } else {
                     app.render('public/views/index.html', 'text/html', httpHandler, 'utf-8');
                 }
-            }).listen(PORT, function () {
-                console.log('Caught ' + PORT + ' out of 151');
+            }).listen(PORT, function() {
+                console.log('-= Upwork server reporting for duty at http://127.0.0.1:' + PORT + ' =-');
             });
     }
 
