ac1a84fe9eecc38dafe0e40e39311df4e745af42
Nicolas Sims <sableye.nico@gmail.com>
2016-07-18 01:03:09 -0400

    Nevermind! That broke everything. Reverting some changes.

--- node/DataHandler.js
+++ node/DataHandler.js
@@ -4,7 +4,8 @@
 
 'use strict';
 
-const nedb = require('nedb'), db = new nedb({filename: './data/proposals-db.json', autoload: true});
+const nedb = require('nedb'),
+    db = new nedb({filename: './data/proposals-db.json', autoload: true});
 this.data = [];
 
 class DataHandler {
@@ -12,11 +13,41 @@ class DataHandler {
         db.loadDatabase();
     }
 
+    loadData(callback) {
+        db.find({completed: '0'}, (err, docs) => {
+            if (docs.length != null) {
+                callback(docs);
+            }
+        });
+    }
+
+    updateData(data) {
+        db.update({_id: data.id}, {
+            building: data.building
+            , roomNumber: data.roomNumber
+            , submitter: data.submitter
+            , problemDesc: data.problemDesc
+            , assigned: data.assigned
+            , completed: data.completed
+            , status: data.status
+            , date: data.date
+        }, {
+            upsert: true,
+            returnUpdatedDocs: true
+        });
+    }
+
     addData(data) {
         db.insert(data, (err) => {
             if (err) {
                 console.log(err);
             }
+        });
+    }
+
+    getDb() {
+        db.find({}, (err, docs) => {
+            return docs;
         });
     }
 }
